70f635b601c0e4d1293a71c87770989cb7b9a088
KENZOBASAR <132684139+KENZOBASAR@users.noreply.github.com>
2024-04-26 21:26:04 +0530

    Add files via upload

---
+++ start.bat
@@ -0,0 +1,2 @@
+java -Xmx1024M -Xms1024M -cp lwjgl.jar;lwjgl_util.jar;XPCRAFT.jar "-Dorg.lwjgl.librarypath=%CD%/natives" "-Dnet.java.games.input.librarypath=%CD%/natives" net.minecraft.client.Minecraft 
+pause
